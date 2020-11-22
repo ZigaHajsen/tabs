2bc3f3652c55f65ae436d00fce58c5a21f54c4e0
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-11-22 20:08:49 +0100

    App component useState, useEffect & fetching

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+const url = 'https://course-api.com/react-tabs-project';
 
-const App = () => {
-  return <div>app</div>;
+const App: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section>
+        <h1>loading...</h1>
+      </section>
+    );
+  }
+
+  return <h2>jobs</h2>;
 };
 
 export default App;
